42daf1ab9a3c2c25010af6b3ea9d1b7223b3a7f7
Christopher Reichert <creichert07@gmail.com>
2013-12-01 17:16:14 -0800

    Call checkaflip GET request in ebay data fetcher.

--- checkaflip-ios/CAFEbayDataFetcher.m
+++ checkaflip-ios/CAFEbayDataFetcher.m
@@ -13,6 +13,18 @@
 - (CAFEbaySearchResult*)search:(NSString*) key
 {
     // Make http request for JSON.
+    NSString* serverAddress = @"http://checkaflip.com/searchEbay/?q=suit&new=false&json=true";
+    NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL: [NSURL URLWithString:serverAddress]
+                                                           cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10];
+    
+    [request setHTTPMethod:@"GET"];
+    
+    NSError* error;
+    NSURLResponse* response = nil;
+    
+    NSData* json = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+    
+    NSString* jsonstr = [[NSString alloc] initWithData:json encoding:NSASCIIStringEncoding];
     return [[CAFEbaySearchResult alloc] init];
 }
 
